cc1dd33aa7299c236445dafbff51d5d3b3bc8dbd
Karol Zając <karolzajac.0407@gmail.com>
2024-10-25 22:43:15 +0200

    fix: test urls

--- WanderWayBackend/views/test/urls.py
+++ WanderWayBackend/views/test/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from WanderWayBackend.views.test.views import SecuredTestView, TestView
 
 urlpatterns = [
-    path('/secured', SecuredTestView.as_view(), name='secured_test'),
-    path('/unsecured', TestView.as_view(), name='unsecured_test'),
+    path('secured', SecuredTestView.as_view(), name='secured_test'),
+    path('unsecured', TestView.as_view(), name='unsecured_test'),
 ]
