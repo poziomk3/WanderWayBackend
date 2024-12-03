6c4d5bbf0dd262ed78f6bf03c89714c5f195b596
therockey <kacperus.tomczyk@gmail.com>
2024-12-03 21:41:57 +0100

    Implement route fetching endpoint

--- WanderWayBackend/views/route/urls.py
+++ WanderWayBackend/views/route/urls.py
@@ -5,5 +5,6 @@ from WanderWayBackend.views.route.views import *
 urlpatterns = [
     path('poi', GetAllPOIs.as_view(), name='get_pois'),
     path('poi/<int:poi_id>/', GetPOI.as_view(), name='get_poi'),
-    path("generate", GenRoutes.as_view(), name='gen_routes')
+    path("generate", GenRoutes.as_view(), name='gen_routes'),
+    path('<int:route_id>/', GetRoute.as_view(), name='get_route'),
 ]
