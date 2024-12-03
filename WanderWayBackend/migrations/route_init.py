5a16775041cfab63a71b6e5cf7699c600673ba1b
therockey <kacperus.tomczyk@gmail.com>
2024-12-03 18:25:27 +0100

    Fix route migration

--- WanderWayBackend/migrations/route_init.py
+++ WanderWayBackend/migrations/route_init.py
@@ -6,10 +6,10 @@ from WanderWayBackend.settings import BASE_DIR
 def load_initial_data(apps, schema_editor):
     route_model = apps.get_model('WanderWayBackend', 'Route')
 
-    if not os.path.exists(BASE_DIR / '/gpx'):
-        os.mkdir(BASE_DIR / '/gpx')
+    if not os.path.exists(BASE_DIR.__str__() + '/gpx'):
+        os.mkdir(BASE_DIR.__str__() + '/gpx')
     else:
-        for file in os.listdir(BASE_DIR / '/gpx'):
+        for file in os.listdir(BASE_DIR.__str__() + '/gpx'):
             if file.endswith('.gpx'):
                 route_model.objects.create(
                     filePath=file
--- WanderWayBackend/views/route/urls.py
+++ WanderWayBackend/views/route/urls.py
@@ -5,4 +5,5 @@ from WanderWayBackend.views.route.views import *
 urlpatterns = [
     path('poi', GetAllPOIs.as_view(), name='get_pois'),
     path('poi/<int:poi_id>/', GetPOI.as_view(), name='get_poi'),
+    path("generate", GenRoutes.as_view(), name='gen_routes')
 ]
