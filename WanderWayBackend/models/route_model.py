efbba7ff6d4a9012a2d84e40234466a0e9015265
therockey <kacperus.tomczyk@gmail.com>
2024-12-06 18:42:36 +0100

    Add image URI field to Route DB model

--- WanderWayBackend/models/route_model.py
+++ WanderWayBackend/models/route_model.py
@@ -5,3 +5,4 @@ class Route(models.Model):
     id = models.AutoField(primary_key=True)
     filePath = models.CharField(max_length=100)
     created_at = models.DateTimeField(auto_now_add=True)
+    imgURI = models.CharField(default=None, blank=True, null=True, max_length=100)
