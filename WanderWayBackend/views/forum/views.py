8ea2558024c291db74ad20d13c3369fed1a7f02c
therockey <kacperus.tomczyk@gmail.com>
2024-12-12 00:48:37 +0100

    Add mussing swagger documentation

--- WanderWayBackend/views/forum/views.py
+++ WanderWayBackend/views/forum/views.py
@@ -1,3 +1,6 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
+from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
@@ -14,31 +17,77 @@ class getPosts(APIView):
     """
     permission_classes = [IsAuthenticated]
 
+    @swagger_auto_schema(
+        responses={200: ForumPostSerializer(many=True)}
+    )
     def get(self, request):
         posts = ForumPost.objects.all()
         serializer = ForumPostSerializer(posts, many=True)
         return Response({'posts': serializer.data})
 
 
-class createPost(APIView):
+class Post(APIView):
     """
         post:
-        Create a new post.
+        Create a new forum post for a route specified with the id.
+
+        get:
+        Get a forum post by the post's id.
     """
     permission_classes = [IsAuthenticated]
 
-    def post(self, request, route_id):
-        route = Route.objects.get(id=route_id)
-        post = ForumPost.objects.create(
-            title=request.data['title'],
-            rating=request.data['rating'],
-            body=request.data['body'],
-            author=request.user,
-            route=route,
-        )
-        return Response({"post_id": post.id}, status=201)
+    @swagger_auto_schema(
+        request_body=ForumPostSerializer,
+        responses={
+            status.HTTP_201_CREATED: openapi.Schema(
+                title="Forum Post ID",
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "post_id": openapi.Schema(
+                        type=openapi.TYPE_INTEGER,
+                        example=1,
+                    ),
+                },
+            ),
+            status.HTTP_404_NOT_FOUND: openapi.Schema(
+                title="Error",
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "error": openapi.Schema(type=openapi.TYPE_STRING, example="Route not found"),
+                },
+            ),
+        },
+    )
+    def post(self, request, id):
+        try:
+            route = Route.objects.get(id=id)
+            post = ForumPost.objects.create(
+                title=request.data['title'],
+                rating=request.data['rating'],
+                body=request.data['body'],
+                author=request.user,
+                route=route,
+            )
+            return Response({"post_id": post.id}, status=201)
+        except Route.DoesNotExist:
+            return Response({"error": "Route not found"}, status=404)
 
-    def get(self, request, route_id):
-        post = ForumPost.objects.get(id=route_id)
-        serializer = ForumPostSerializer(post)
-        return Response(serializer.data)
+    @swagger_auto_schema(
+        responses={
+            status.HTTP_200_OK: ForumPostSerializer,
+            status.HTTP_404_NOT_FOUND: openapi.Schema(
+                title="Error",
+                type=openapi.TYPE_OBJECT,
+                properties={
+                    "error": openapi.Schema(type=openapi.TYPE_STRING, example="Post not found"),
+                },
+            ),
+        },
+    )
+    def get(self, request, id):
+        try:
+            post = ForumPost.objects.get(id=id)
+            serializer = ForumPostSerializer(post)
+            return Response(serializer.data)
+        except ForumPost.DoesNotExist:
+            return Response({"error": "Post not found"}, status=404)
