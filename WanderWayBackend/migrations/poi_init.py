80d400f007bd4960d5402fd74d20719d1b7cb6bc
Kacper Tomczyk <37952104+therockey@users.noreply.github.com>
2025-01-08 15:08:15 +0100

    Google Places Photo fetching for POIs (#26)
    * implement google places api image fetching
    
    * Fix test
    
    * Fix issue where the city's main google POI always came first in list of nearby places

--- WanderWayBackend/migrations/poi_init.py
+++ WanderWayBackend/migrations/poi_init.py
@@ -8,133 +8,114 @@ def load_initial_data(apps, schema_editor):
         description="Późnogotycki budynek na wrocławskim Rynku, jeden z najlepiej zachowanych historycznych ratuszy w Polsce, zarazem jeden z głównych zabytków architektonicznych Wrocławia.",
         latitude=51.109562,
         longitude=17.031937,
-        imgURI = "placeholder.jpg"
     )
     poi_model.objects.create(
         name="Katerdra św. Jana Chrzciciela",
         description="Rzymskokatolicki gotycki kościół parafialny, położony na Ostrowie Tumskim - najstarszej części Wrocławia, położonej na lewym brzegu Odry.",
         latitude=51.114187,
         longitude=17.046563,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Hala Stulecia",
         description="Hala widowiskowo-sportowa we Wrocławiu, ekspresjonistyczna, wzniesiona w latach 1911–1913 według projektu Maxa Berga.",
         latitude=51.106938,
         longitude=17.077312,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Ogród Japoński",
         description="Ogród japoński znajdujący się w Parku Szczytnickim we Wrocławiu. Stanowi połączenie kilku typów ogrodów japońskich: publicznego, wodnego, związanego z ceremonią picia herbaty oraz kamienistej plaży.",
         latitude=51.109687,
         longitude=17.079062,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Zoo Wrocław",
         description="Ogród zoologiczny znajdujący się przy ul. Wróblewskiego 1–5 we Wrocławiu, otwarty 10 lipca 1865. Jest najstarszym na obecnych ziemiach polskich ogrodem zoologicznym w Polsce.",
         latitude=51.105688,
         longitude=17.076188,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Sky Tower",
         description="Neomodernistyczny wieżowiec we Wrocławiu, stanowiący wraz z dwoma sąsiadującymi budynkami kompleks mieszkalny, biurowy, handlowo-usługowy i rekreacyjny. Jest najwyższym budynkiem mieszkalnym we Wrocławiu i jednym z najwyższych w Polsce.",
         latitude=51.094562,
         longitude=17.018937,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Dworzec Świebodzki",
         description="Nieczynna stacja kolejowa w ścisłym centrum Wrocławia przy placu Orląt Lwowskich; najstarszy z trzech zachowanych do dziś pasażerskich dworców kolejowych we Wrocławiu.",
         latitude=51.108062,
         longitude=17.020063,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Galeria Neon Side",
         description="Najjaśniejsze podwórko Wrocławia - Ruska 46, rozświetlone zabytkowymi neonami, to miejsce skupiające organizacje kulturalne, galerie sztuki, zespoły muzyczne, kluby, teatry, organizacje pozarządowe i artystów freelancerów, którzy mają tu swoje siedziby lub pracownie.",
         latitude=51.109938,
         longitude=17.024563,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Hala Targowa",
         description="Hala targowa we Wrocławiu, zlokalizowana przy ul. Piaskowej 17, w pobliżu Rynku.",
         latitude=51.112562,
         longitude=17.039813,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Most Grunwaldzki",
         description="Most drogowy wiszący nad Odrą we Wrocławiu, zbudowany w latach 1908-1910. Początkowo most nosił nazwę Mostu Cesarskiego, a następnie Mostu Wolności.",
         latitude=51.109437,
         longitude=17.052563,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Muzeum Narodowe",
         description="Jedno z głównych muzeów Wrocławia i Dolnego Śląska. Zbiory muzeum obejmują przede wszystkim malarstwo i rzeźbę, ze szczególnym uwzględnieniem sztuki całego Śląska.",
         latitude=51.110937,
         longitude=17.047688,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Wrocławska Fontanna Multimedialna",
         description="Fontanna we Wrocławiu, otoczona pergolą, przy Hali Stulecia. Jest największą fontanną w Polsce i jedną z największych w Europie.",
         latitude=51.108812,
         longitude=17.078938,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Plac Nowy Targ",
         description="Jeden z trzech historycznych placów targowych Starego Miasta we Wrocławiu, obok Rynku i placu Solnego.",
         latitude=51.111312,
         longitude=17.038312,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Narodowe Forum Muzyki im. Witolda Lutosławskiego",
         description="Jeden z największych obiektów koncertowych w Europie mieszczący się we Wrocławiu oraz nazwa instytucji kultury powstałej w 2014 roku.",
         latitude=51.107062,
         longitude=17.026438,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Panorama Racławicka",
         description="Muzeum sztuki we Wrocławiu, oddział Muzeum Narodowego we Wrocławiu, założone w 1893 we Lwowie, od 1980 we Wrocławiu.",
         latitude=51.110312,
         longitude=17.044938,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Bastion Sakwowy",
         description="Fragment dawnych fortyfikacji miasta Wrocławia, w południowo-wschodnim ich narożniku. W latach 1948–2024 wzgórze nosiło nazwę Wzgórze Partyzantów.",
         latitude=51.104813,
         longitude=17.038562,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Teatr Muzyczny „Capitol”",
         description="Teatr muzyczny powstały we Wrocławiu w 2004 roku, będący kontynuacją wcześniejszych teatrów istniejących w tym samym miejscu takich jak: Operetka Dolnośląska (1955 – 1976), Teatr Muzyczny (1977 – 1979), Operetka Wrocławska (1979 – 1991) oraz Teatr Muzyczny – Operetka Wrocławska (1991 – 2004).",
         latitude=51.100562,
         longitude=17.029812,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Pałac Królewski we Wrocławiu",
         description="Zespół pałacowy stanowiący od XVIII do XX wieku wrocławską rezydencję pruskich królów z dynastii Hohenzollernów, obecnie siedziba Muzeum Miejskiego Wrocławia.",
         latitude=51.107437,
         longitude=17.028813,
-        imgURI="placeholder.jpg"
     )
     poi_model.objects.create(
         name="Gmach główny Uniwersytetu Wrocławskiego",
         description="Barokowy budynek o długości 171 metrów, zbudowany w XVIII wieku na miejscu dawnego zamku piastowskiego. Powstał z inicjatywy cesarza Leopolda I jako siedziba Akademii Leopoldyńskiej.",
         latitude=51.114062,
         longitude=17.034438,
-        imgURI="placeholder.jpg"
     )
 
 
--- WanderWayBackend/views/route/lib.py
+++ WanderWayBackend/views/route/lib.py
@@ -9,14 +9,21 @@ from WanderWayBackend.models.route_model import Route
 from WanderWayBackend.settings import BASE_DIR
 
 
-def get_route_obj(route_id):
+def get_route_obj(route_id: int):
     try:
         return Route.objects.get(id=route_id), "Ok"
     except Route.DoesNotExist:
         return None, "Route not found"
 
 
-def get_route_file(route):
+def get_poi_obj(poi_id: int):
+    try:
+        return POI.objects.get(id=poi_id), "Ok"
+    except POI.DoesNotExist:
+        return None, "POI not found"
+
+
+def get_route_file(route: Route):
     file_name = route.filePath
     file_path = os.path.join(BASE_DIR, 'gpx', file_name)
     try:
@@ -26,21 +33,24 @@ def get_route_file(route):
         return None, None, "Route file not found on server"
 
 
-def get_poi_img(poi_id):
+def get_poi_img(poi_id: int):
+    poi, op_status = get_poi_obj(poi_id)
+    if not poi: return None, None, op_status
+    filename = f"{poi.id}.jpg"
+    img_path = os.path.join(BASE_DIR, 'images', 'poi', filename)
+
+    if not os.path.exists(img_path):
+        if not _generate_poi_img(poi):
+            return None, None, "Failed to generate image"
+
     try:
-        poi = POI.objects.get(id=poi_id)
-        filename = poi.imgURI
-        img_path = os.path.join(BASE_DIR, 'images', 'poi', filename)
-        try:
-            img_file = open(img_path, 'rb')
-            return img_file, filename, "Ok"
-        except FileNotFoundError:
-            return None, None, "Image file not found on server"
-    except POI.DoesNotExist:
-        return None, None, "POI not found"
+        img_file = open(img_path, 'rb')
+        return img_file, filename, "Ok"
+    except FileNotFoundError:
+        return None, None, "Image file not found on server"
 
 
-def get_route_img(route_id, img_type):
+def get_route_img(route_id: int, img_type: str):
     route, op_status = get_route_obj(route_id)
     if not route: return None, None, op_status
     filename = f"{route.id}_{img_type}.jpg"
@@ -57,11 +67,11 @@ def get_route_img(route_id, img_type):
         return None, None, "Image file not found on server"
 
 
-def _encode_polyline(points):
+def _encode_polyline(points: list) -> str:
     return polyline.encode(points)
 
 
-def _encode_markers(pois):
+def _encode_markers(pois: list) -> str:
     marker_strs = [
         f"&markers=label:{poi[2]}|{poi[0]},{poi[1]}"
         for poi in pois
@@ -69,30 +79,55 @@ def _encode_markers(pois):
     return "".join(marker_strs)
 
 
-def _generate_google_maps_url(encoded_polyline, encoded_markers, img_type, size="1280x720"):
+def _generate_google_maps_url(encoded_polyline: str, encoded_markers: str, img_type: str, size="1280x720") -> str:
     return f"https://maps.googleapis.com/maps/api/staticmap?size={size}{encoded_markers}&style=feature:poi|visibility:off&path=color:red|weight:10|enc:{encoded_polyline}&key={config('GOOGLE_MAPS_API_KEY')}&maptype={img_type}"
 
 
-def _generate_route_img(route, img_type):
+def _generate_poi_img(poi: POI) -> bool:
+    search_url = f"https://maps.googleapis.com/maps/api/place/nearbysearch/json?location={poi.latitude},{poi.longitude}&radius=200&key={config('GOOGLE_MAPS_API_KEY')}"
+    response = requests.get(search_url)
+    if response.status_code != 200:
+        return False
+
+    data = response.json()
+    if not data['results']:
+        return False
+
+    photo_ref = None
+
+    for place in data['results'][1:]:
+        if 'photos' in place.keys():
+            photo_ref = place['photos'][0]['photo_reference']
+            break
+
+    if not photo_ref:
+        return False
+
+    img_url = f"https://maps.googleapis.com/maps/api/place/photo?maxwidth=400&photoreference={photo_ref}&key={config('GOOGLE_MAPS_API_KEY')}"
+
+    return _save_img(img_url, os.path.join(BASE_DIR, 'images', 'poi', f'{poi.id}.jpg'))
+
+
+def _generate_route_img(route: Route, img_type: str) -> bool:
     route_file, filename, op_status = get_route_file(route)
     if not route_file: return False
     points, pois = _parse_gpx(route_file)
     url = _generate_google_maps_url(_encode_polyline(points), _encode_markers(pois), img_type)
-    return _save_route_img(url, route.id, img_type)
+    return _save_img(url, os.path.join(BASE_DIR, 'images', 'route', f'{route.id}_{img_type}.jpg'))
 
 
-def _save_route_img(url, route_id, img_type):
-    save_path = os.path.join(BASE_DIR, 'images', 'route', f'{route_id}_{img_type}.jpg')
+def _save_img(url: str, save_path: str) -> bool:
     response = requests.get(url, stream=True)
-    if response.status_code == 200:
-        os.makedirs(os.path.dirname(save_path), exist_ok=True)
-
-        with open(save_path, 'wb') as file:
-            for chunk in response.iter_content(chunk_size=8192):
-                file.write(chunk)
-        return True
-    else:
+    if response.status_code != 200:
         return False
+    print("Image fetched... saving image to", save_path)
+
+    os.makedirs(os.path.dirname(save_path), exist_ok=True)
+
+    with open(save_path, 'wb') as file:
+        for chunk in response.iter_content(chunk_size=8192):
+            file.write(chunk)
+    return True
 
 
 def _parse_gpx(file):
